3a6eb945bb6828296a2ce82e6daf195a2621836b
KessaPassa <k.someya.gyao@gmail.com>
2018-07-31 16:02:27 +0900

    apiサーバだけ

--- main.mjs
+++ main.mjs
@@ -1,60 +1,60 @@
-// import * as env from "./secret/env";
+// // import * as env from "./secret/env";
 let env = process.env;
-import * as bot from './src/SetupBotkit';
-let controller = bot.setup();
-
+// import * as bot from './src/SetupBotkit';
+// let controller = bot.setup();
+//
 // データベース
 import * as database from './src/Database';
 database.setup();
-
-
-// メモ機能
-import * as memo from './src/Memo';
-controller.hears(['add (.*)'], 'direct_message,direct_mention,mention', function (bot, message) {
-    memo.add(bot, message);
-});
-
-controller.hears(['list'], 'direct_message,direct_mention,mention', function (bot, message) {
-    memo.list(bot, message);
-});
-
-controller.hears(['remove (.*)'], 'direct_message,direct_mention,mention', function (bot, message) {
-    memo.remove(bot, message);
-});
-
-
-// 在室管理
-import * as room from './src/Room';
-controller.hears(['login'], 'direct_mention,mention', function (bot, message) {
-    room.login(bot, message);
-});
-
-controller.hears(['logout'], 'direct_mention,mention', function (bot, message) {
-    room.logout(bot, message);
-});
-
-controller.hears(['room'], 'direct_mention,mention', function (bot, message) {
-    room.room(bot, message);
-});
-
-import schedule from 'node-schedule';
-schedule.scheduleJob({
-    hour: 5,
-    minute: 0
-}, function () {
-    room.forceLogout();
-});
-
-
-// 分報機能
-import * as timeline from './src/Timeline';
-controller.hears(['(.*)'], 'ambient', function (bot, message) {
-    timeline.chat(bot, message);
-});
-
-controller.on(['file_shared'], function (bot, message) {
-    timeline.file(bot, message);
-});
+//
+//
+// // メモ機能
+// import * as memo from './src/Memo';
+// controller.hears(['add (.*)'], 'direct_message,direct_mention,mention', function (bot, message) {
+//     memo.add(bot, message);
+// });
+//
+// controller.hears(['list'], 'direct_message,direct_mention,mention', function (bot, message) {
+//     memo.list(bot, message);
+// });
+//
+// controller.hears(['remove (.*)'], 'direct_message,direct_mention,mention', function (bot, message) {
+//     memo.remove(bot, message);
+// });
+//
+//
+// // 在室管理
+// import * as room from './src/Room';
+// controller.hears(['login'], 'direct_mention,mention', function (bot, message) {
+//     room.login(bot, message);
+// });
+//
+// controller.hears(['logout'], 'direct_mention,mention', function (bot, message) {
+//     room.logout(bot, message);
+// });
+//
+// controller.hears(['room'], 'direct_mention,mention', function (bot, message) {
+//     room.room(bot, message);
+// });
+//
+// import schedule from 'node-schedule';
+// schedule.scheduleJob({
+//     hour: 5,
+//     minute: 0
+// }, function () {
+//     room.forceLogout();
+// });
+//
+//
+// // 分報機能
+// import * as timeline from './src/Timeline';
+// controller.hears(['(.*)'], 'ambient', function (bot, message) {
+//     timeline.chat(bot, message);
+// });
+//
+// controller.on(['file_shared'], function (bot, message) {
+//     timeline.file(bot, message);
+// });
 
 
 // APIサーバ機能
