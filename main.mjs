ae635e3d4fd58a9c5d525d906661e372668c02d5
KessaPassa <k.someya.gyao@gmail.com>
2018-08-06 09:15:18 +0900

    これでhttps解決なはず

--- main.mjs
+++ main.mjs
@@ -1,61 +1,64 @@
-// import * as env from "./secret/env";
-// let env = process.env;
-// import * as bot from './src/SetupBotkit';
-// let controller = bot.setup();
-//
+import * as bot from './src/SetupBotkit';
+
+let controller = bot.setup();
+
 // データベース
 import * as database from './src/Database';
 
 database.setup();
-//
-//
-// // メモ機能
-// import * as memo from './src/Memo';
-// controller.hears(['add (.*)'], 'direct_message,direct_mention,mention', function (bot, message) {
-//     memo.add(bot, message);
-// });
-//
-// controller.hears(['list'], 'direct_message,direct_mention,mention', function (bot, message) {
-//     memo.list(bot, message);
-// });
-//
-// controller.hears(['remove (.*)'], 'direct_message,direct_mention,mention', function (bot, message) {
-//     memo.remove(bot, message);
-// });
-//
-//
-// // 在室管理
-// import * as room from './src/Room';
-// controller.hears(['login'], 'direct_mention,mention', function (bot, message) {
-//     room.login(bot, message);
-// });
-//
-// controller.hears(['logout'], 'direct_mention,mention', function (bot, message) {
-//     room.logout(bot, message);
-// });
-//
-// controller.hears(['room'], 'direct_mention,mention', function (bot, message) {
-//     room.room(bot, message);
-// });
-//
-// import schedule from 'node-schedule';
-// schedule.scheduleJob({
-//     hour: 5,
-//     minute: 0
-// }, function () {
-//     room.forceLogout();
-// });
-//
-//
-// // 分報機能
-// import * as timeline from './src/Timeline';
-// controller.hears(['(.*)'], 'ambient', function (bot, message) {
-//     timeline.chat(bot, message);
-// });
-//
-// controller.on(['file_shared'], function (bot, message) {
-//     timeline.file(bot, message);
-// });
+
+
+// メモ機能
+import * as memo from './src/Memo';
+
+controller.hears(['add (.*)'], 'direct_message,direct_mention,mention', function (bot, message) {
+    memo.add(bot, message);
+});
+
+controller.hears(['list'], 'direct_message,direct_mention,mention', function (bot, message) {
+    memo.list(bot, message);
+});
+
+controller.hears(['remove (.*)'], 'direct_message,direct_mention,mention', function (bot, message) {
+    memo.remove(bot, message);
+});
+
+
+// 在室管理
+import * as room from './src/Room';
+
+controller.hears(['login'], 'direct_mention,mention', function (bot, message) {
+    room.login(bot, message);
+});
+
+controller.hears(['logout'], 'direct_mention,mention', function (bot, message) {
+    room.logout(bot, message);
+});
+
+controller.hears(['room'], 'direct_mention,mention', function (bot, message) {
+    room.room(bot, message);
+});
+
+import schedule from 'node-schedule';
+
+schedule.scheduleJob({
+    hour: 5,
+    minute: 0
+}, function () {
+    room.forceLogout();
+});
+
+
+// 分報機能
+import * as timeline from './src/Timeline';
+
+controller.hears(['(.*)'], 'ambient', function (bot, message) {
+    timeline.chat(bot, message);
+});
+
+controller.on(['file_shared'], function (bot, message) {
+    timeline.file(bot, message);
+});
 
 
 // APIサーバ機能
@@ -68,6 +71,23 @@ const app = express();
 app.set('port', (process.env.PORT || 8010));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+// get  の時は response.send
+// post の時は response.json
+
+// 在室情報
+app.post('/room/info', function (request, response) {
+    serverApi.getInfo(request, response);
+});
+
+// login, logout管理
+app.post('/room/management', function (request, response) {
+    serverApi.sendInfo(request, response);
+});
+
+app.listen(app.get('port'), function () {
+    console.log('server launched');
+});
 
 // app.use("/", (function () {
 //     let router = express.Router();
@@ -91,70 +111,3 @@ app.use(bodyParser.json());
 //
 //     return router;
 // })());
-
-// 在室情報
-app.post('/room/info', function (request, response) {
-    serverApi.getInfo(request, response);
-});
-
-// login, logout管理
-app.post('/room/management', function (request, response) {
-    serverApi.sendInfo(request, response);
-});
-
-app.get('/hoge', function(request, response) {
-    response.json({
-        status: 'だだだだだだだ'
-    });
-});
-
-app.post('/hoge2', function(request, response) {
-    response.send('This is Twitter-bot application.');
-});
-
-app.post('/hoge3', function(request, response) {
-    response.json({
-        status: 'だだだだだだだ'
-    });
-});
-
-app.listen(app.get('port'), function(){
-    console.log('server launched');
-});
-
-// let db;
-//
-// // Connect to the database before starting the application server.
-// mongodb.MongoClient.connect(env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
-//     if (err) {
-//         console.log(err);
-//         process.exit(1);
-//     }
-//
-//     // Save database object from the callback for reuse.
-//     db = client.db();
-//     console.log("Database connection ready");
-//
-//     // Initialize the app.
-//     let server = app.listen(8010, function () {
-//         let port = server.address().port;
-//         console.log("App now running on port", port);
-//     });
-// });
-//
-// import * as serverApi from "./src/ReceiveServer";
-// // 在室情報
-// app.post("/room/info", function(request, response) {
-//     serverApi.getInfo(request, response);
-// });
-//
-// // login, logout管理
-// app.post("/room/management", function(request, response) {
-//     serverApi.sendInfo(request, response);
-// });
-//
-// app.get("/room", function(request, response) {
-//     response.json({
-//         status: 'okokok'
-//     });
-// });
